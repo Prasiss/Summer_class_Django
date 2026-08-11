dfcb25b04cce7075900e3faf7769f3aaa3b17a06
Prasiss <165300233+Prasiss@users.noreply.github.com>
2026-08-11 09:46:24 +0545

    added new pages to my existing design

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products, name='products'),        
-    path('<int:id>/',views.product_details, name='product_detail'),
-    path('addtocart/',views.add_to_cart, name='add_to_cart'),
-    path('checkout/',views.checkout, name='checkout'),
-]   
+    path('', views.products, name='products'),
+    path('<int:id>/', views.product_details, name='product_detail'),
+
+    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('add-product/', views.add_product, name='add_product'),
+    path('edit-product/<int:id>/', views.edit_product, name='edit_product'),
+
+    path('my-orders/', views.my_orders, name='my_orders'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('my-sales/', views.my_sales, name='my_sales'),
+
+    path('change-password/', views.change_password, name='change_password'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+
+    path('my-request/', views.sent_messages, name='sent_messages'),
+    path('my-request-received/', views.message_details, name='message_details'),
+
+    path('sent/', views.sent, name='sent'),
+]
--- products/views.py
+++ products/views.py
@@ -17,3 +17,50 @@ def add_to_cart(request):
 
 def checkout(request):
     return render(request, 'main/checkout.html')
+
+def dashboard(request):
+    return render(request, 'main/accounts/dashboard.html')
+
+
+def add_product(request):
+    return render(request, 'main/accounts/add-product.html')
+
+
+def edit_product(request):
+    return render(request, 'main/accounts/edit-product.html')
+
+
+def my_orders(request):
+    return render(request, 'main/accounts/my-orders.html')
+
+
+def inbox(request):
+    return render(request, 'main/accounts/inbox.html')
+
+
+def my_sales(request):
+    return render(request, 'main/accounts/my-sales.html')
+
+
+def change_password(request):
+    return render(request, 'main/accounts/change-password.html')
+
+
+def my_profile(request):
+    return render(request, 'main/accounts/edit-profile.html')
+
+
+def sent_messages(request):
+    return render(request, 'main/accounts/my-request.html')
+
+
+def message_details(request):
+    return render(request, 'main/accounts/my-request-received.html')
+
+
+def edit_profile(request):
+    return render(request, 'main/accounts/edit_profile.html')
+
+
+def sent(request):
+    return render(request, 'main/accounts/sent.html')
