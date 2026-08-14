5e78490f6c03fc96b41bda89be600a86f6dd45a4
Prasiss <165300233+Prasiss@users.noreply.github.com>
2026-08-14 07:27:56 +0545

    added new files and updated code for html files

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from products.models import Product
+from products.models import Product,Category
 from blog.models import Blog
 
 def home(request):
     products = Product.objects.all()
     blogs = Blog.objects.all()
-    return render (request, 'main/home.html', {'products':products, 'blogs':blogs})
+    cateogories=Category.objects.all().filter(status=True)
+    banner=Product.objects.all().filter(status=True)
+    return render (request, 'main/home.html', {'products':products, 'blogs':blogs, 'categories':cateogories, 'banner':banner})
